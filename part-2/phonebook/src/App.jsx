d655ca9e58a5d41fa072011e3fa27c48e1766215
Rimmi Bhadani <rimmibhadani2004@gmail.com>
2025-12-28 13:42:53 +0200

    partial 2b

---
+++ part-2/phonebook/src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])  
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  
+
+
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const existingPerson = persons.find(person => person.name === newName)
+    if (existingPerson) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    const personObject = {
+      name: newName,
+      number: newNumber 
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+    
+  } 
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchCHange = (event) => {
+    console.log(event.target.value)
+    setShowAll(event.target.value)
+  }
+
+  const personsToShow = showAll
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(showAll.toLowerCase()))
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div> 
+        filter shown with <input values={showAll} onChange={handleSearchCHange}/>
+      </div>
+      <div>
+          <button type="submit">search</button>
+        </div>
+      <h2>Add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        <ul>
+          {persons.map(person => <li key={person.name}>{person.name} - {person.number}</li>)}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
